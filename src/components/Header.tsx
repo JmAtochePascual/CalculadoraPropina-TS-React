83a1e65a399c201756dea0915381606ab62eca12
JeanCarlos <jmatochepascual@gmail.com>
2024-11-25 13:05:14 -0500

    feat: :sparkles: Agrega el componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+
+export const Header = () => {
+  return (
+    <h1 className="py-8 text-center text-2xl font-extrabold bg-teal-400 md:text-4xl">
+      Calculadora de Propinas y Consumos
+    </h1>
+  );
+};
